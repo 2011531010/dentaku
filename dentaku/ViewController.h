76431b302f3e4a1a86ae27b1803f373fb2cbb01a
2011531010 <2011531010@ma246.is.env.kitakyu-u.ac.jp>
2013-11-14 16:00:41 +0900

    電卓のボタンの作成

--- dentaku/ViewController.h
+++ dentaku/ViewController.h
@@ -8,6 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *resultLabel;
+    int count;
+}
+@property (nonatomic, assign) NSInteger buttonNumber;
+@property (nonatomic, assign) NSInteger countNumber;
+
+- (IBAction)pushedClearButton:(id)sender;
+- (IBAction)pushedDividButton:(id)sender;
+- (IBAction)pushedMultiplyButton:(id)sender;
+- (IBAction)pushedDeleatButton:(id)sender;
+- (IBAction)pushedSubButton:(id)sender;
+- (IBAction)pushedSumButton:(id)sender;
+- (IBAction)pushedEqualButton:(id)sender;
+- (IBAction)pushedPointButton:(id)sender;
+- (IBAction)pushedZeroButton:(id)sender;
+- (IBAction)pushedOneButton:(id)sender;
+- (IBAction)pushedTwoButton:(id)sender;
+- (IBAction)pushedThreeButton:(id)sender;
+- (IBAction)pushedFourButton:(id)sender;
+- (IBAction)pushedFiveButton:(id)sender;
+- (IBAction)pushedSixButton:(id)sender;
+- (IBAction)pushedSevenButton:(id)sender;
+- (IBAction)pushedEightButton:(id)sender;
+- (IBAction)pushedNineButton:(id)sender;
+
+
 
 @end
--- dentaku/ViewController.m
+++ dentaku/ViewController.m
@@ -14,9 +14,14 @@
 
 @implementation ViewController
 
+@synthesize buttonNumber;
+
+@synthesize countNumber;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    count = 0;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +31,73 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pushedClearButton:(id)sender {
+}
+
+- (IBAction)pushedDividButton:(id)sender {
+}
+
+- (IBAction)pushedMultiplyButton:(id)sender {
+}
+
+- (IBAction)pushedDeleatButton:(id)sender {
+}
+
+- (IBAction)pushedSubButton:(id)sender {
+}
+
+- (IBAction)pushedSumButton:(id)sender {
+    
+    count++;
+    resultLabel.text = [NSString stringWithFormat:@"%d", count];
+}
+
+- (IBAction)pushedEqualButton:(id)sender {
+}
+
+- (IBAction)pushedPointButton:(id)sender {
+}
+
+- (IBAction)pushedZeroButton:(id)sender {
+}
+
+- (IBAction)pushedOneButton:(id)sender {
+    
+    self.countNumber = (self.countNumber * 10) + 1;
+    NSString *print = [[NSString alloc] initWithFormat:@"%d", self.countNumber];
+    resultLabel.text = print;
+    
+    }
+
+- (IBAction)pushedTwoButton:(id)sender {
+    
+   self.countNumber = (self.countNumber * 10) + 2;
+    NSString *print = [[NSString alloc] initWithFormat:@"%d", self.countNumber];
+    resultLabel.text = print;
+    
+}
+
+- (IBAction)pushedThreeButton:(id)sender {
+}
+
+- (IBAction)pushedFourButton:(id)sender {
+}
+
+- (IBAction)pushedFiveButton:(id)sender {
+}
+
+- (IBAction)pushedSixButton:(id)sender {
+}
+
+- (IBAction)pushedSevenButton:(id)sender {
+}
+
+- (IBAction)pushedEightButton:(id)sender {
+}
+
+- (IBAction)pushedNineButton:(id)sender {
+}
+
+- (IBAction)waru:(id)sender {
+}
 @end
