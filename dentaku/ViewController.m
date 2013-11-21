d443c2649042b03f8c4ee9e9e835be25b1e6c877
2011531010 <2011531010@ma246.is.env.kitakyu-u.ac.jp>
2013-11-21 16:04:54 +0900

    計算の作成（足し算以外バグ有り）

--- dentaku/ViewController.m
+++ dentaku/ViewController.m
@@ -12,16 +12,23 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    int value;
+    int calcFlug;
+}
 
 @synthesize buttonNumber;
 
 @synthesize countNumber;
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    count = 0;
+    value = 0;
+    self.countNumber = 0;
+    calcFlug = 0;
+    self.buttonNumber = 0;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -32,72 +39,172 @@
 }
 
 - (IBAction)pushedClearButton:(id)sender {
+    
+    value = 0;
+    self.countNumber = 0;
+    calcFlug =0;
+    self.buttonNumber = 0;
+    
+    [self labelOutput];
 }
 
 - (IBAction)pushedDividButton:(id)sender {
+    
+    value /= self.countNumber;
+    self.countNumber = 0;
+    calcFlug = 4;
+    NSLog(@"value:%d",value);
 }
 
 - (IBAction)pushedMultiplyButton:(id)sender {
+    
+    value *= self.countNumber;
+    self.countNumber = 0;
+    calcFlug = 3;
+    NSLog(@"value:%d",value);
+    
 }
 
 - (IBAction)pushedDeleatButton:(id)sender {
 }
 
 - (IBAction)pushedSubButton:(id)sender {
+    
+    value -= self.countNumber;
+    self.countNumber = 0;
+    calcFlug = 2;
+    NSLog(@"value:%d",value);
 }
 
 - (IBAction)pushedSumButton:(id)sender {
     
-    count++;
-    resultLabel.text = [NSString stringWithFormat:@"%d", count];
+    value += self.countNumber;
+    self.countNumber = 0;
+    calcFlug = 1;
+    NSLog(@"value:%d",value);
 }
 
 - (IBAction)pushedEqualButton:(id)sender {
+    NSLog(@"value:%d",value);
+    switch (calcFlug) {
+            
+        case 1: //足し算
+            value += self.countNumber;
+            break;
+            
+        case 2: //引き算
+            value -= self.countNumber;
+            break;
+            
+        case 3: //かけ算
+            value *= self.countNumber;
+            break;
+            
+        case 4: //割り算
+            value /= self.countNumber;
+            break;
+            
+        default:
+            break;
+    }
+    
+    
+    NSString *print = [[NSString alloc] initWithFormat:@"%d", value];
+    resultLabel.text = print;
+    
 }
 
 - (IBAction)pushedPointButton:(id)sender {
 }
 
 - (IBAction)pushedZeroButton:(id)sender {
-}
+    
+    self.buttonNumber = 0;
+    [self labelOutput];
+    }
 
 - (IBAction)pushedOneButton:(id)sender {
     
-    self.countNumber = (self.countNumber * 10) + 1;
-    NSString *print = [[NSString alloc] initWithFormat:@"%d", self.countNumber];
-    resultLabel.text = print;
+    self.buttonNumber = 1;
+    
+    [self labelOutput];
     
     }
 
 - (IBAction)pushedTwoButton:(id)sender {
     
-   self.countNumber = (self.countNumber * 10) + 2;
+    self.buttonNumber = 2;
+    
+    [self labelOutput];
+}
+
+
+- (IBAction)pushedThreeButton:(id)sender {
+    
+    self.buttonNumber = 3;
+    
+    [self labelOutput];
+}
+
+- (IBAction)pushedFourButton:(id)sender {
+    
+    self.buttonNumber = 4;
+    
+    [self labelOutput];
+}
+
+- (IBAction)pushedFiveButton:(id)sender {
+    
+    self.buttonNumber = 5;
+    
+    [self labelOutput];
+    
+}
+
+- (IBAction)pushedSixButton:(id)sender {
+    
+    self.buttonNumber = 6;
+    
+    [self labelOutput];
+    
+}
+
+- (IBAction)pushedSevenButton:(id)sender {
+    
+    self.buttonNumber = 7;
+    
+    [self labelOutput];
+    
+}
+
+- (IBAction)pushedEightButton:(id)sender {
+    
+    self.buttonNumber = 8;
+    
+    [self labelOutput];
+    
+}
+
+- (IBAction)pushedNineButton:(id)sender {
+    
+    self.buttonNumber = 9;
+    
+    [self labelOutput];
+    
+}
+
+
+- (void)labelOutput{
+    
+    self.countNumber = (self.countNumber * 10) + self.buttonNumber;
     NSString *print = [[NSString alloc] initWithFormat:@"%d", self.countNumber];
     resultLabel.text = print;
     
 }
 
-- (IBAction)pushedThreeButton:(id)sender {
-}
-
-- (IBAction)pushedFourButton:(id)sender {
-}
-
-- (IBAction)pushedFiveButton:(id)sender {
-}
-
-- (IBAction)pushedSixButton:(id)sender {
-}
-
-- (IBAction)pushedSevenButton:(id)sender {
-}
-
-- (IBAction)pushedEightButton:(id)sender {
-}
-
-- (IBAction)pushedNineButton:(id)sender {
-}
 
 - (IBAction)waru:(id)sender {
 }
+
+
 @end
